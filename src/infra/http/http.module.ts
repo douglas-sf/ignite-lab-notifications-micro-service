6aef09f2b6907a87ef1f93b43ba213ba53b2ed68
Douglas Ferreira <douglas_dsferreira@hotmail.com>
2022-12-16 10:00:02 -0300

    paths fixed

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { SendNotification } from '../../app/use-cases/send-notification';
-import { DatabaseModule } from '../database/database.module';
 
+import { SendNotification } from '@app/use-cases/send-notification';
+import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
 
 @Module({
